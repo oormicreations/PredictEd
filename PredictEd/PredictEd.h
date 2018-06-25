a3539d05d5b7f386fa504919c2dc7b90e46fc7d7
Sanjeev <oormicreations@gmail.com>
2018-06-26 02:38:59 +0530

    Add project files.

---
+++ PredictEd/PredictEd.h
@@ -0,0 +1,32 @@
+
+// PredictEd.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CPredictEdApp:
+// See PredictEd.cpp for the implementation of this class
+//
+
+class CPredictEdApp : public CWinApp
+{
+public:
+	CPredictEdApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPredictEdApp theApp;
